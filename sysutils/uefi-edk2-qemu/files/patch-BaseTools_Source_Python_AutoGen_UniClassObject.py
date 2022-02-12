7916298a80a3c798716342617f86d67feb523b05
Eric Camachat <eric@camachat.org>
2022-02-12 14:59:09 -0600

    sysutils/uefi-edk2-qemu: fix build with python 3.9+

---
+++ sysutils/uefi-edk2-qemu/files/patch-BaseTools_Source_Python_AutoGen_UniClassObject.py
@@ -0,0 +1,11 @@
+--- BaseTools/Source/Python/AutoGen/UniClassObject.py.orig	2019-11-25 08:14:18.000000000 -0600
++++ BaseTools/Source/Python/AutoGen/UniClassObject.py	2022-01-21 10:55:01.309389000 -0600
+@@ -309,7 +309,7 @@ class UniFileClassObject(object):
+ 
+     @staticmethod
+     def VerifyUcs2Data(FileIn, FileName, Encoding):
+-        Ucs2Info = codecs.lookup('ucs-2')
++        Ucs2Info = codecs.lookup('utf-16')
+         #
+         # Convert to unicode
+         #
---
+++ sysutils/uefi-edk2-qemu/files/patch-BaseTools_Source_Python_Common_Misc.py
@@ -0,0 +1,20 @@
+--- BaseTools/Source/Python/Common/Misc.py.orig	2022-01-21 10:56:26.425526000 -0600
++++ BaseTools/Source/Python/Common/Misc.py	2022-01-21 11:00:47.051534000 -0600
+@@ -1635,9 +1635,14 @@ class PeImageClass():
+         ByteArray = array.array('B')
+         ByteArray.fromfile(PeObject, 4)
+         # PE signature should be 'PE\0\0'
+-        if ByteArray.tostring() != b'PE\0\0':
+-            self.ErrorInfo = self.FileName + ' has no valid PE signature PE00'
+-            return
++        if sys.version_info < (3,9):
++            if ByteArray.tostring() != b'PE\0\0':
++                self.ErrorInfo = self.FileName + ' has no valid PE signature PE00'
++                return
++        else:
++            if ByteArray.tobytes() != b'PE\0\0':
++                self.ErrorInfo = self.FileName + ' has no valid PE signature PE00'
++                return
+ 
+         # Read PE file header
+         ByteArray = array.array('B')
