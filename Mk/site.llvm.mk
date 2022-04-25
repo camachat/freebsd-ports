82e4cc157c5cb082c33ca5bc391d2387f7776ceb
Eric Camachat <eric@camachat.org>
2022-04-25 16:50:06 -0500

    Mk/site.llvm.mk: improved.

--- Mk/site.llvm.mk
+++ Mk/site.llvm.mk
@@ -10,13 +10,29 @@ BINARY_ALIAS+= 	cpp=${LOCALBASE}/bin/clang-cpp${LLVM_VERSION} \
                 c++=${LOCALBASE}/bin/clang++${LLVM_VERSION} \
                 ar=${LOCALBASE}/bin/llvm-ar${LLVM_VERSION} \
                 nm=${LOCALBASE}/bin/llvm-nm${LLVM_VERSION} \
-                ld=${LOCALBASE}/bin/ld.lld${LLVM_VERSION}
+                ld=${LOCALBASE}/bin/ld.lld${LLVM_VERSION} \
+                objcopy=${LOCALBASE}/bin/llvm-objcopy${LLVM_VERSION} \
+                objdump=${LOCALBASE}/bin/llvm-objdump${LLVM_VERSION} \
+                readobj=${LOCALBASE}/bin/llvm-readobj${LLVM_VERSION} \
+                ranlib=${LOCALBASE}/bin/llvm-ranlib${LLVM_VERSION} \
+                readelf=${LOCALBASE}/bin/llvm-readelf${LLVM_VERSION} \
+                size=${LOCALBASE}/bin/llvm-size${LLVM_VERSION} \
+                strings=${LOCALBASE}/bin/llvm-strings${LLVM_VERSION} \
+                strip=${LOCALBASE}/bin/llvm-strip${LLVM_VERSION}
 
-CPP=    ${LOCALBASE}/bin/clang-cpp${LLVM_VERSION}
-CC=     ${LOCALBASE}/bin/clang${LLVM_VERSION}
-CXX=    ${LOCALBASE}/bin/clang++${LLVM_VERSION}
-AR=     ${LOCALBASE}/bin/llvm-ar${LLVM_VERSION}
-NM=     ${LOCALBASE}/bin/llvm-nm${LLVM_VERSION}
-LD=     ${LOCALBASE}/bin/ld.lld${LLVM_VERSION}
+CPP     = ${LOCALBASE}/bin/clang-cpp${LLVM_VERSION}
+CC      = ${LOCALBASE}/bin/clang${LLVM_VERSION}
+CXX     = ${LOCALBASE}/bin/clang++${LLVM_VERSION}
+AR      = ${LOCALBASE}/bin/llvm-ar${LLVM_VERSION}
+NM      = ${LOCALBASE}/bin/llvm-nm${LLVM_VERSION}
+LD      = ${LOCALBASE}/bin/ld.lld${LLVM_VERSION}
+OBJCOPY = ${LOCALBASE}/bin/llvm-objcopy${LLVM_VERSION}
+OBJDUMP = ${LOCALBASE}/bin/llvm-objdump${LLVM_VERSION}
+READOBJ = ${LOCALBASE}/bin/llvm-readobj${LLVM_VERSION}
+RANLIB  = ${LOCALBASE}/bin/llvm-ranlib${LLVM_VERSION}
+READELF = ${LOCALBASE}/bin/llvm-readelf${LLVM_VERSION}
+SIZE    = ${LOCALBASE}/bin/llvm-size${LLVM_VERSION}
+STRINGS = ${LOCALBASE}/bin/llvm-strings${LLVM_VERSION}
+STRIP   = ${LOCALBASE}/bin/llvm-strip${LLVM_VERSION}
 .endif # _SITE_LLVM_MK_INCLUDED
 
