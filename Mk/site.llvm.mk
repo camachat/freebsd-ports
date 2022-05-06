37fcd87735ae82b5a6eebb830a0773a3e61604e9
Eric Camachat <eric@camachat.org>
2022-05-06 09:38:18 -0500

    Mk/site.llvm.mk: improved.

--- Mk/site.llvm.mk
+++ Mk/site.llvm.mk
@@ -26,13 +26,13 @@ CXX     = ${LOCALBASE}/bin/clang++${LLVM_VERSION}
 AR      = ${LOCALBASE}/bin/llvm-ar${LLVM_VERSION}
 NM      = ${LOCALBASE}/bin/llvm-nm${LLVM_VERSION}
 LD      = ${LOCALBASE}/bin/ld.lld${LLVM_VERSION}
-OBJCOPY = ${LOCALBASE}/bin/llvm-objcopy${LLVM_VERSION}
-OBJDUMP = ${LOCALBASE}/bin/llvm-objdump${LLVM_VERSION}
-READOBJ = ${LOCALBASE}/bin/llvm-readobj${LLVM_VERSION}
-RANLIB  = ${LOCALBASE}/bin/llvm-ranlib${LLVM_VERSION}
-READELF = ${LOCALBASE}/bin/llvm-readelf${LLVM_VERSION}
-SIZE    = ${LOCALBASE}/bin/llvm-size${LLVM_VERSION}
-STRINGS = ${LOCALBASE}/bin/llvm-strings${LLVM_VERSION}
-STRIP   = ${LOCALBASE}/bin/llvm-strip${LLVM_VERSION}
+#OBJCOPY = ${LOCALBASE}/bin/llvm-objcopy${LLVM_VERSION}
+#OBJDUMP = ${LOCALBASE}/bin/llvm-objdump${LLVM_VERSION}
+#READOBJ = ${LOCALBASE}/bin/llvm-readobj${LLVM_VERSION}
+#RANLIB  = ${LOCALBASE}/bin/llvm-ranlib${LLVM_VERSION}
+#READELF = ${LOCALBASE}/bin/llvm-readelf${LLVM_VERSION}
+#SIZE    = ${LOCALBASE}/bin/llvm-size${LLVM_VERSION}
+#STRINGS = ${LOCALBASE}/bin/llvm-strings${LLVM_VERSION}
+#STRIP   = ${LOCALBASE}/bin/llvm-strip${LLVM_VERSION}
 .endif # _SITE_LLVM_MK_INCLUDED
 
