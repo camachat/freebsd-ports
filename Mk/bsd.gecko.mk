fd9e58439ef2daea76da999eea9070c343e11eb7
Eric Camachat <eric@camachat.org>
2023-09-24 20:36:34 -0500

    Mk/bsd.gecko.mk: default to LLVM17.

--- Mk/bsd.gecko.mk
+++ Mk/bsd.gecko.mk
@@ -102,8 +102,8 @@ MOZ_EXPORT+=	LLVM_OBJDUMP="${LOCALBASE}/bin/llvm-objdump${LLVM_DEFAULT}"
 # fix LLVM to version 13, as that's the only reasonable wasi-toolchain
 # we currently have
 #    if !defined(DEFAULT_VERSIONS) || ! ${DEFAULT_VERSIONS:Mllvm*} || ${PORT_OPTIONS:MLTO}
-LLVM_DEFAULT?=	13 # chase bundled LLVM in lang/rust for LTO
-LLVM_VERSION?=	13.0.1 # keep in sync with devel/wasi-compiler-rt${LLVM_DEFAULT}
+LLVM_DEFAULT?=	17 # chase bundled LLVM in lang/rust for LTO
+LLVM_VERSION?=	17.0.1 # keep in sync with devel/wasi-compiler-rt${LLVM_DEFAULT}
 #    endif
 # Require newer Clang than what's in base system unless user opted out
 .    if ${CC} == cc && ${CXX} == c++ && exists(/usr/lib/libc++.so)
