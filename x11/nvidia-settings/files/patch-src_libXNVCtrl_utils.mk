b988e60a66938666b1cee6a8691e54b10f414107
Eric Camachat <eric@camachat.org>
2024-06-07 07:49:09 -0500

    x11/nvidia-settings: fix build on 14.1-RELEASE.

--- x11/nvidia-settings/files/patch-src_libXNVCtrl_utils.mk
+++ x11/nvidia-settings/files/patch-src_libXNVCtrl_utils.mk
@@ -15,7 +15,7 @@
  	$(at_if_quiet)$$(MKDIR) $$(OUTPUTDIR)
  	$(at_if_quiet)cd $$(dir $(1)); \
 -	$$(call quiet_cmd_no_at,LD) -r -z noexecstack --format=binary \
-+	$$(call quiet_cmd_no_at,LD) -m elf_$(TARGET_ARCH) -r -z noexecstack --format=binary \
++	$$(call quiet_cmd_no_at,LD) -m elf_$(if $(filter-out amd64,$(TARGET_ARCH)),$(TARGET_ARCH),x86_64) -r -z noexecstack --format=binary \
  	    $$(notdir $(1)) -o $$(OUTPUTDIR_ABSOLUTE)/$$(notdir $$@)
  	$$(call quiet_cmd,OBJCOPY) \
  	    --rename-section .data=.rodata,contents,alloc,load,data,readonly \
