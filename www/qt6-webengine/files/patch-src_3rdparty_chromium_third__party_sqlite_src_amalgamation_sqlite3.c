94f3f55fd84f01eefa0df8cff1178872e999b4c0
Eric Camachat <eric@camachat.org>
2024-06-07 12:45:47 -0500

    www/qt6-webengine: workaround on 14.1-RELEASE amd64.

--- www/qt6-webengine/files/patch-src_3rdparty_chromium_third__party_sqlite_src_amalgamation_sqlite3.c
+++ www/qt6-webengine/files/patch-src_3rdparty_chromium_third__party_sqlite_src_amalgamation_sqlite3.c
@@ -1,6 +1,17 @@
---- src/3rdparty/chromium/third_party/sqlite/src/amalgamation/sqlite3.c.orig	2023-08-17 07:33:31 UTC
+--- src/3rdparty/chromium/third_party/sqlite/src/amalgamation/sqlite3.c.orig	2024-05-15 13:57:21 UTC
 +++ src/3rdparty/chromium/third_party/sqlite/src/amalgamation/sqlite3.c
-@@ -43843,7 +43843,12 @@ static int unixRandomness(sqlite3_vfs *NotUsed, int nB
+@@ -20107,8 +20107,8 @@ SQLITE_PRIVATE int sqlite3HeapNearlyFull(void);
+ ** that deal with sqlite3StackAlloc() failures to be unreachable.
+ */
+ #ifdef SQLITE_USE_ALLOCA
+-# define sqlite3StackAllocRaw(D,N)   alloca(N)
+-# define sqlite3StackAllocRawNN(D,N) alloca(N)
++# define sqlite3StackAllocRaw(D,N)   __builtin_alloca(N)
++# define sqlite3StackAllocRawNN(D,N) __builtin_alloca(N)
+ # define sqlite3StackFree(D,P)
+ # define sqlite3StackFreeNN(D,P)
+ #else
+@@ -43844,7 +43844,12 @@ static int unixRandomness(sqlite3_vfs *NotUsed, int nB
    memset(zBuf, 0, nBuf);
    randomnessPid = osGetpid(0);
  #if !defined(SQLITE_TEST) && !defined(SQLITE_OMIT_RANDOMNESS)
@@ -13,7 +24,7 @@
      int fd, got;
      fd = robust_open("/dev/urandom", O_RDONLY, 0);
      if( fd<0 ){
-@@ -43858,6 +43863,7 @@ static int unixRandomness(sqlite3_vfs *NotUsed, int nB
+@@ -43859,6 +43864,7 @@ static int unixRandomness(sqlite3_vfs *NotUsed, int nB
        robust_close(0, fd, __LINE__);
      }
    }
