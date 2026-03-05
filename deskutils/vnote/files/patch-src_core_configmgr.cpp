09298d712a301354b7959c13e9a1b94d4a78e853
Eric Camachat <eric@camachat.org>
2026-03-05 16:44:14 -0600

    deskutils/vnote: update to 3.20.1

--- deskutils/vnote/files/patch-src_core_configmgr.cpp
+++ deskutils/vnote/files/patch-src_core_configmgr.cpp
@@ -1,11 +1,11 @@
---- src/core/configmgr.cpp.orig	2024-08-06 13:56:46 UTC
+--- src/core/configmgr.cpp.orig	2025-11-26 12:50:22 UTC
 +++ src/core/configmgr.cpp
-@@ -576,7 +576,7 @@ void ConfigMgr::initAppPrefixPath()
-     QStringList potential_dirs;
-     auto app_dir_path = QCoreApplication::applicationDirPath();
-     qInfo() << "app prefix path: " << app_dir_path;
--    potential_dirs << app_dir_path;
-+    potential_dirs << app_dir_path << "%%DATADIR%%";
+@@ -509,7 +509,7 @@ void ConfigMgr::initAppPrefixPath() {
+   QStringList potential_dirs;
+   auto app_dir_path = QCoreApplication::applicationDirPath();
+   qInfo() << "app prefix path: " << app_dir_path;
+-  potential_dirs << app_dir_path;
++  potential_dirs << app_dir_path << "%%DATADIR%%";
  
  #if defined(Q_OS_LINUX)
-     QDir localBinDir(app_dir_path);
+   QDir localBinDir(app_dir_path);
