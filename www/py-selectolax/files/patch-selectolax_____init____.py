48a1f091dce8efa34d4ef1e92c48b3b5f5b14d7b
Eric Camachat <eric@camachat.org>
2026-04-04 22:02:54 -0500

    www/py-selectolax: new port

---
+++ www/py-selectolax/files/patch-selectolax_____init____.py
@@ -0,0 +1,8 @@
+--- selectolax/__init__.py.orig	2026-04-04 03:54:41 UTC
++++ selectolax/__init__.py
+@@ -5,4 +5,4 @@ __version__ = "0.4.7"
+ __email__ = "me@rushter.com"
+ __version__ = "0.4.7"
+ 
+-from . import lexbor, modest, parser
++from . import lexbor
